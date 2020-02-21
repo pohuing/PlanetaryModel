d32797e92d7f0b19e99794c8af2048ef7caa7c63
pohuing <24683548+pohuing@users.noreply.github.com>
2020-02-21 04:19:36 +0100

    Allowed for proper resizing of the viewport

--- Application/src/Engine/Engine.cpp
+++ Application/src/Engine/Engine.cpp
@@ -1,6 +1,7 @@
 #include "Engine.h"
-constexpr int SCR_WIDTH = 800;
-constexpr int SCR_HEIGHT = 800;
+static auto SCR_WIDTH = 800;
+static auto SCR_HEIGHT = 800;
+static auto FRAMEBUFER_SIZE_CHANGED = false;
 
 void message_callback(GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length, const GLchar* message, const void* userParam) {
 	fprintf(stderr, "GL CALLBACK: %s type = 0x%x, severity = 0x%x, message = %s\n",
@@ -12,6 +13,9 @@ void message_callback(GLenum source, GLenum type, GLuint id, GLenum severity, GL
 void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
 	// make sure the viewport matches the new window dimensions; note that width and
 	// height will be significantly larger than specified on retina displays.
+	SCR_WIDTH = width;
+	SCR_HEIGHT = height;
+	FRAMEBUFER_SIZE_CHANGED = true;
 	glViewport(0, 0, width, height);
 }
 
@@ -81,16 +85,18 @@ void Engine::AddEntity(std::unique_ptr<IBody> entity) {
 void Engine::Mainloop() {
 	double mouse_x = 0, mouse_y = 0, old_mouse_x = 0, old_mouse_y = 0, mouse_diff_x = 0, mouse_diff_y = 0;
 	float old_time = 0, delta_time = 0;
+	auto projection = glm::perspective(glm::radians(90.f), (float)SCR_WIDTH / SCR_HEIGHT, 0.1f, 1000.f);
+	auto view = glm::lookAt(glm::vec3(2, 0, 0), glm::vec3(0, 0, 0), glm::vec3(0, 1, 0));
+	
 	m_shaderprogramm.Bind();
-	m_shaderprogramm.SetViewMatrix(glm::lookAt(glm::vec3(2, 0, 0), glm::vec3(0, 0, 0), glm::vec3(0, 1, 0)));
-	glm::mat4 projection = glm::perspective(glm::radians(90.f), (float)SCR_WIDTH / SCR_HEIGHT, 0.1f, 1000.f);
+	m_shaderprogramm.SetViewMatrix(view);
 	m_shaderprogramm.SetProjectionMatrix(projection);
 	m_shaderprogramm.SetLightColor(glm::vec3(0, 1, 0));
 	m_shaderprogramm.SetLightPosition(glm::vec3(10, 10, 10));
 	m_shaderprogramm.SetObjectColor(glm::vec3(1, 1, 1));
 
 	m_sun_shader.Bind();
-	m_sun_shader.SetViewMatrix(glm::lookAt(glm::vec3(2, 0, 0), glm::vec3(0, 0, 0), glm::vec3(0, 1, 0)));
+	m_sun_shader.SetViewMatrix(view);
 	m_sun_shader.SetProjectionMatrix(projection);
 
 	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
@@ -100,6 +106,12 @@ void Engine::Mainloop() {
 
 	while(!glfwWindowShouldClose(m_window)) {
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		if (FRAMEBUFER_SIZE_CHANGED)
+		{
+			projection = glm::perspective(glm::radians(90.f), (float)SCR_WIDTH / SCR_HEIGHT, 0.1f, 1000.f);
+			m_shaderprogramm.SetProjectionMatrix(projection);
+			FRAMEBUFER_SIZE_CHANGED = false;
+		}
 		glfwGetCursorPos(m_window, &mouse_x, &mouse_y);
 		mouse_diff_x = mouse_x - old_mouse_x;
 		mouse_diff_y = mouse_y - old_mouse_y;
